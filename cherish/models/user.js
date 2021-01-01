ede0140dc42013e31ca114acf968c424cb37b347
YeongEunLee <zduddmszz#gmail.com>
2021-01-01 22:43:20 +0900

    chore

--- cherish/models/user.js
+++ cherish/models/user.js
@@ -38,7 +38,7 @@ module.exports = (sequelize, DataTypes) => {
         }
     }, {
         timestamps: false,
-        underscored: false,
+        underscored: true,
         tableName: 'user',
     })
 }
